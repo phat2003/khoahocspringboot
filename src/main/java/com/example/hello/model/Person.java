af685ef141d8fc5975b92e3e0c04e0e2c1e0b998
phat2003 <phamanhtienphat@gmail.com>
2024-07-22 10:42:54 +0700

    .

--- src/main/java/com/example/hello/model/Person.java
+++ src/main/java/com/example/hello/model/Person.java
@@ -5,28 +5,28 @@ public class Person {
 	private String firstName;
 	private String lastName;
 	
-	public Person() {
+	public Person() {//khai báo đối tượng Person
 		
 	}
 	
-	public Person(String firstName, String lastName) {
-		this.firstName = firstName;
-		this.lastName = lastName;
+	public Person(String firstName, String lastName) { //cũng khai báo đối tượng Person nhưng truyền vào cho đối tượng Person 2 biến firstName, lastName (2 biến này là của đối tượng Person)
+		this.firstName = firstName;//chỉ định biến private firstName khai báo ở lên  = biến firstName của Person( điều này có nghĩa là chỉ cần ghi firstName thì đều sẽ là biến của đối tượng này lẫn biến khai báo ở trên) 
+		this.lastName = lastName;//tương tự như firstName.
 	}
 	
-	public String getFirstName() {
-		return firstName;
+	public String getFirstName() {//khai báo 1 biến getFirstName(lấy firstname)
+		return firstName;//biến này sẽ trả về firstName
 	}
 	
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
+	public void setFirstName(String firstName) {//khai báo 1 biến setFirstName(đặt firstname)
+		this.firstName = firstName;//cho biến private khai báo ở bên và biến bên method này là như nhau khi gọi
 	}
 	
-	public String getLastName() {
+	public String getLastName() {//tương tự như getFirstName
 		return lastName;
 	}
 	
-	public void setLastName(String lastName) {
+	public void setLastName(String lastName) {//tương tự như setFirstName
 		this.lastName = lastName;
 	}
 
